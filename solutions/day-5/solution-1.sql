a3500417e26ed9ef7c1cbf50e348aa650c520072
David Sillman <david.sillman@intusurg.com>
2024-12-05 07:43:02 -0800

    complete day 5 part 1

--- solutions/day-5/solution-1.sql
+++ solutions/day-5/solution-1.sql
@@ -16,6 +16,7 @@ categorized_lines as (
 
 ordering_rules as (
     select
+        line_number as rule_id,
         split_part(line_content, '|', 1) as first_page,
         split_part(line_content, '|', 2) as second_page
     from categorized_lines
@@ -24,7 +25,7 @@ ordering_rules as (
 
 page_updates as (
     select
-        line_number,
+        line_number as update_id,
         unnest(
             string_to_array(line_content, ',')
         ) as page_num
@@ -34,26 +35,96 @@ page_updates as (
 
 indexed_page_updates as (
     select
-        line_number,
-        row_number() over (partition by line_number) as page_index,
+        update_id,
+        row_number() over (partition by update_id) as page_index,
         page_num
     from page_updates
 ),
 
-joint_with_relevant_ordering_rules as (
+ordered_pairs_with_relevance as (
     select
-        page_update.line_number,
+        page_update.update_id,
         page_update.page_index,
         page_update.page_num,
+        rule.rule_id,
         rule.first_page,
-        rule.second_page
+        rule.second_page,
+        count(*) 
+            over (partition by update_id, rule_id) = 2 
+            as is_relevant
     from indexed_page_updates as page_update
     join ordering_rules as rule
         on page_update.page_num = rule.first_page
         or page_update.page_num = rule.second_page
+),
+
+rules_pairs_in_updates as (
+    select
+        update_id,
+        rule_id,
+        first_page as rule_first_page,
+        second_page as rule_second_page,
+        first_value(page_num) 
+            over (
+                partition by update_id, rule_id
+                order by page_index
+            ) as update_first_page,
+        last_value(page_num) 
+            over (
+                partition by update_id, rule_id
+                order by page_index
+            ) as update_second_page
+    from ordered_pairs_with_relevance
+    where is_relevant
+    order by update_id, rule_id
+),
+
+rules_in_updates_with_correctness as (
+    select
+        update_id,
+        rule_id,
+        rule_first_page,
+        rule_second_page,
+        update_first_page,
+        update_second_page,
+        (
+            rule_first_page = update_first_page and
+            rule_second_page = update_second_page
+        ) as is_correct
+    from rules_pairs_in_updates
+    -- knock out irrelevant member of pair
+    where update_first_page != update_second_page
+),
+
+completely_correct_update_ids as (
+    select
+        update_id
+    from rules_in_updates_with_correctness
+    group by update_id
+    having bool_and(is_correct)
+),
+
+completely_correct_updates_pages as (
+    select
+        idx_updates.update_id,
+        idx_updates.page_index,
+        idx_updates.page_num,
+        max(idx_updates.page_index) 
+            over (partition by idx_updates.update_id)
+            as max_page_index
+    from indexed_page_updates as idx_updates
+    inner join completely_correct_update_ids
+        using (update_id)
+),
+
+completely_correct_update_midpoints as (
+    select
+        update_id,
+        page_num as midpoint_page
+    from completely_correct_updates_pages
+    where page_index = (max_page_index + 1) / 2
 )
 
-select *
-from joint_with_relevant_ordering_rules
-limit 10000
+select sum(midpoint_page :: int) as answer
+from completely_correct_update_midpoints
 ;
